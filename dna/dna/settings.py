d61d35be6a5c7fe5d24219f7cdab5df0e4132dd9
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-03-27 16:05:23 +0100

    Adds dna_profile object

--- dna/dna/settings.py
+++ dna/dna/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'dna.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['dna/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

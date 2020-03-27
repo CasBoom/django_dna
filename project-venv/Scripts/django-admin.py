2c23a1705a2abc40d3fecffa563b4dc4f306ae11
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-03-27 12:24:24 +0100

    Venv setup

---
+++ project-venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\casbo\Desktop\files\School\jaar2\per7\Web\pandas\project-venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

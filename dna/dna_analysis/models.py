b6e40a8dc60dae45ef55122d9cb85a4f61613ae6
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-04-22 15:37:48 +0200

    adds a function that returns the first protein string in te RNA

--- dna/dna_analysis/models.py
+++ dna/dna_analysis/models.py
@@ -83,6 +83,26 @@ class dna_profile(models.Model):
                 rna = rna + letter
         return rna
 
+    def generate_protein(self):
+        rna = self.find_rna()
+        codon_table = {'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V', 'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V', 'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V', 'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V', 'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A', 'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A', 'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A', 'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A', 'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D', 'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D', 'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E', 'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E', 'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G', 'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G', 'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G', 'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'}
+
+        output_protein = ""
+        protein_piece = ""
+        reading=False
+        for i in range(0, len(rna), 3):
+            protein_piece = rna[i:i+3]
+            if(protein_piece == "AUG" or reading):
+                reading=True
+                for key, value in codon_table.items():    
+                    if(rna[i:i+3] != protein_piece.replace(key, value)):
+                        protein_piece = protein_piece.replace(key, value)
+                if(protein_piece == "Stop"):
+                    break
+                output_protein += protein_piece
+
+        return output_protein
+
     def __str__(self):
         return self.title
 
